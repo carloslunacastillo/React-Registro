62cc5f8855df923337af54846438305ad0cba241
carloslunacastillo <carl_dharius@hotmail.com>
2019-01-26 00:12:46 -0600

    Delete database.js

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = 'mongodb://localhost/mern-task';
-
-mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
-
-    module.exports = mongoose;
